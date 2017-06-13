075ea9356de60417874a710af1aaa2b6fdf09d05
kimromi <hiromikimura@pepabo.com>
2017-06-13 17:29:43 +0900

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in ruboty-page_speed_insights.gemspec
+gemspec
---
+++ lib/ruboty/page_speed_insights.rb
@@ -0,0 +1,7 @@
+require "ruboty/page_speed_insights/version"
+
+module Ruboty
+  module PageSpeedInsights
+    # Your code goes here...
+  end
+end
